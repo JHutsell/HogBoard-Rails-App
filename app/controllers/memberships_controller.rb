44156b89a92c3165cb7aedfbd0ba3f7d9aa61fac
Kevin Lai <kevinlai76@gmail.com>
2019-07-09 11:04:17 -0400

    membership/course seeds

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,23 @@
 class MembershipsController < ApplicationController
+    
+    def index
+        @memberships = @current_student.memberships
+    end
+
+    def new
+        @membership = Membership.new
+        @clubs = Club.all
+    end
+
+    def create
+        @membership = Membership.create(student_id: @student_id, membership_params)
+        redirect_to student_path(@student_id)
+    end
+
+    private
+
+    def membership_params
+        params.require(:membership).permit(:club_id)
+    end
 end
+
