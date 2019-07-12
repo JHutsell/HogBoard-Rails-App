06eab90f838aa667f87298e600539bdf675c38c4
github username <github email address>
2019-07-12 10:06:51 -0400

    background pics

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,5 @@ class Membership < ApplicationRecord
     belongs_to :student
     belongs_to :club
 
-    validates_uniqueness_of :club_id, :scope => [:student_id], on: :create
+    # validates_uniqueness_of :club_id, :scope => [:student_id], on: :create
 end
